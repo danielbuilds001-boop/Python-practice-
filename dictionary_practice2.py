42aa25fb9241f07e75304851592912d2008f8e04
danielbuilds001-boop <daniel.builds001@gmail.com>
2026-08-29 22:29:31 +0200

    A few weak spots but I'll fix them

---
+++ dictionary_practice2.py
@@ -0,0 +1,53 @@
+def add_movies(movies):
+    title = input("What is the name of the movie (q to quit): ").strip().title()
+    if title.lower() == "q":
+        return False
+
+    if not title:
+        print("The name of the movie cannot be empty.")
+        return True
+
+    try:
+        year = int(input("Enter the year the movie released: ").strip())
+        rating = int(input("Enter the movie rating: ").strip())
+
+    except ValueError:
+        print("Invalid input please try again.")
+        return True
+
+    movies[title] = {
+        "year": year,
+        "rating": rating
+    }
+    print(f'Successfully added {title}')
+    return True
+
+def display_movies(movies):
+    if not movies:
+        print("There are no movies avaliable.")
+        return True
+
+    for title,details in movies.items():
+        print(f'\n{title}')
+        print(f'Year: {details["year"]}')
+        print(f'Rating: {details["rating"]}/10')
+
+def main():
+    movies = {}
+    while True:
+        keep_going = add_movies(movies)
+        
+        if not keep_going:
+            break
+        if not movies:
+            break
+            
+            return
+        display_movies(movies)
+        
+        return
+
+if __name__ == '__main__':
+    main()
+    
+Finally also any other useful termux packages
